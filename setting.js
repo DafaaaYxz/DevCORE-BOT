e19b19f4c17f595034b925946dd0b772e47274dc
DafaaaYxz <zeuz8686@gmail.com>
2025-12-23 23:30:46 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // INFO BOT
+    botToken: "8569011860:AAExiVYMdr3_3kWkva8jF4SdTobsiFUMPrE",
+    ownerId: 7341190291, // ID Telegram kamu (cek di @userinfobot)
+    
+    // DEFAULT PERSONA
+    defaultAiName: "DevCORE",
+    defaultOwnerName: "XdpzQ",
+    
+    // DATABASE UPSTASH
+    upstashUrl: "https://top-starling-47196.upstash.io",
+    upstashToken: "AbhcAAIncDE3OGVhMWE4YjU0ZGY0ZmJlOThjYTJiODM5ZThmZTY0ZnAxNDcxOTY",
+    
+    // OPENROUTER
+    openRouterModel: "deepseek/deepseek-chat" // Bisa diganti
+};
